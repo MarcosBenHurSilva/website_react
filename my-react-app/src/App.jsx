d9a7ce4d63a28d0f843f6b618b3e7b8553de987a
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-25 19:40:13 -0300

    Conditional rendering

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,26 +1,16 @@
-// props =  read-only properties that are shared between components.
-//                A parent component can send data to a child component.
-//                key=value
+// Conditional rendering = allows you to control what gets rendered 
+//                         in your application based on certain conditions. 
+//                         (ex. show, hide, or change components)
 
-// propTypes = a mechanism that ensures that the passed value
-//                        is of the correct datatype.
-//                       age: PropTypes.number
-
-
-// defaultProps = default values for props in case they are not
-//                            passed from the parent component
-//                            name: "Guest"
-
-import Student from "./Student";
+import UserGreeting from './UserGreeting.jsx';
 
 function App() {
     return(
       <>
-        <Student name="Spoongebob" age={30} isStudent={true}/>
-        <Student name="Patrick" age={41} isStudent={false}/>
-        <Student name="Squidward" age={50} isStudent={false}/>
-        <Student name="Sandy" age={27} isStudent={true}/>
-        <Student age={20}/>
+        <UserGreeting isLoggedIn={true} username="Marcos"/>
+        <UserGreeting/>
+        <UserGreeting isLoggedIn={true}/>
+        <UserGreeting username="Marcos"/>
       </>
     );
 }
