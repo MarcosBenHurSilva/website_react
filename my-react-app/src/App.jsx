7e40c058f0009eeed9727b13fb7e5bf950e6f309
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-11 17:00:23 -0300

    Button component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,23 +1,18 @@
 import List from './List.jsx'
 
+// HOW TO STYLE REACT COMPONENTS WITH CSS
+//----------------------------------------
+// (not including external frameworks or preproccessors)
+
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INLINE
+
+import Button from './Button.jsx';
+
 function App() {
 
-  const fruits = [{id: 1, name: "apple", calories: 95}, 
-                  {id: 2,name:"orange", calories: 45}, 
-                  {id: 3,name:"banana", calories: 105}, 
-                  {id: 4,name:"coconut", calories: 159}, 
-                  {id: 5,name:"pineapple", calories: 37}];
-
-  const vegetables = [{id: 6, name: "potatos", calories: 110}, 
-                      {id: 7,name:"celery", calories: 15}, 
-                      {id: 8,name:"carrots", calories: 25}, 
-                      {id: 9,name:"corn", calories: 63}, 
-                      {id: 10,name:"broccoli", calories: 50}];
-    
-  return(<>
-          {fruits.length > 0 && <List items={fruits} category="Fruits"/>}
-          {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
-        </>);
+    return(<Button/>);
 }
 
 export default App
