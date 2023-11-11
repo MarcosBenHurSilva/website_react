eb114b678f7169c347bc750ceff5027901e0fb6b
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-11 18:09:47 -0300

    events

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,17 +1,8 @@
 
 function Button() {
 
-    let count = 0;
+    const handleClick = (e) => e.target.textContent = "OUCH!";
 
-    const handleClick = (name) => {
-        if(count < 3){
-            count++;
-            console.log(`${name} you clicked me ${count} time/s`);
-        } else{
-            console.log(`${name} stop clicking me`);
-        }
-    };
-
-    return (<button onClick={() => handleClick("Marcos")}>Click me 😄</button>);
+    return (<button onClick={(e) => handleClick(e)}>Click me 😄</button>);
 }
 export default Button
