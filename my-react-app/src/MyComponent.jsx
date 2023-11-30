51beeb3e4079cbebee960a6e063197a370074d23
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-30 15:40:46 -0300

    onChange event handler

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,25 +1,68 @@
+// onChange = Event handler used primarily with form elements.
+//                       ex. <input>, <textarea>, <select>, <radio>
+//                       Triggers a function every time the value of the input changes.
+
 import React, {useState} from 'react';
 
 function MyComponent(){
-    const [name, setName] = useState("Guest");
-    const [age, setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
-    const updateName = () => {
-        setName("Spongebob");
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState();
+    const [comment, setComment] = useState("");
+    const [payment, setPayment] = useState("");
+    const [shipping, setShipping] = useState("");
+
+    function handleNameChange(event){
+        setName(event.target.value);
     }
-    const incrementAge = () => {
-        setAge(age + 1);
+
+    function handleQuantityChange(event){
+        setQuantity(event.target.value);
     }
-    const toggleEmployedStatus = () => {
-        setIsEmployed(!isEmployed);
+
+    function handleCommentChange(event){
+        setComment(event.target.value);
     }
-    return( <div>
-                <p>Name: {name}</p>
-                <button onClick={updateName}>Set Name</button>
-                <p>Age: {age}</p>
-                <button onClick={incrementAge}>Increment Age</button>
-                <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
-                <button onClick={toggleEmployedStatus}>Toggle Status</button>
-            </div>);
+
+    function handlePaymentChange(event){
+        setPayment(event.target.value);
+    }
+
+    function handleShippingChange(event){
+        setShipping(event.target.value);
+    }
+
+    return(<div>
+            <input value={name} onChange={handleNameChange} placeholder='Guest'/>
+            <p>Name: {name}</p>
+
+            <input value={quantity} onChange={handleQuantityChange} type="number" placeholder='0'/>
+            <p>Quantity: {quantity}</p>
+
+            <textarea value={comment} onChange={handleCommentChange}
+            placeholder="Enter delivery instructions"/>
+            <p>Comment: {comment}</p>
+
+            <select value={payment} onChange={handlePaymentChange}>
+                <option value="">Select an option:</option>
+                <option value="Visa">Visa</option>
+                <option value="MasterCard">MasterCard</option>
+                <option value="GiftCard">GiftCard</option>       
+            </select>
+            <p>Payment: {payment}</p>
+
+            <label>
+                <input type='radio' value="Pick Up"
+                       checked={shipping === "Pick Up"}
+                       onChange={handleShippingChange}/>
+                Pick Up
+            </label>
+            <label>
+            <input type='radio' value="Delivery"
+                       checked={shipping === "Delivery"}
+                       onChange={handleShippingChange}/>
+                Delivery
+            </label>
+            <p>Shipping: {shipping}</p>
+        </div>);
 }
 export default MyComponent
